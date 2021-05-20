316d7ce6d9254f8ffff558be70e4e0309e0656d6
Nikita Filonov <filonov.nikitkaa@gmail.com>
2021-05-20 10:49:27 +0300

    main screen added

--- src/components/items/Event.js
+++ src/components/items/Event.js
@@ -1,17 +1,45 @@
-import React from "react";
+import React, {useState} from "react";
 import {Form, ListGroup} from "react-bootstrap";
-import {Pencil, Trash} from 'react-bootstrap-icons';
+import {ArrowRight, CheckLg, Pencil, Trash} from 'react-bootstrap-icons';
 
-export const Event = ({item, updateEvent, deleteEvent}) =>
-    <ListGroup.Item className="list-group-item d-flex justify-content-between align-items-center">
-        <Form.Check
-            type="checkbox"
-            onChange={() => updateEvent(item.id, 'done', !item.done)}
-            checked={item.done}
-            label={item.title}
-        />
-        <Pencil className="right ml-auto"/>
-        <Trash onClick={() => deleteEvent(item.id)} className={'ml-3'}/>
-    </ListGroup.Item>
+export const Event = ({item, updateEvent, deleteEvent}) => {
+    const [edit, setEdit] = useState(false)
+    const [title, setTitle] = useState(item.title)
 
+    return (
+        <ListGroup.Item className="list-group-item d-flex justify-content-between align-items-center">
+            {edit
+                ? <>
+                    <Form.Control
+                        value={title}
+                        onChange={event => setTitle(event.target.value)}
+                        type="text"
+                        className={'mr-3'}
+                        placeholder="I need to..."
+                    />
+                    <CheckLg
+                        onClick={() => {
+                            title.length > 0 && updateEvent(item.id, 'title', title)
+                            setEdit(false)
+                        }}
+                        className="right ml-auto"
+                    />
+                    <ArrowRight onClick={() => {
+                        setEdit(false)
+                        setTitle(item.title)
+                    }} className={'ml-3'}/>
+                </>
+                : <>
+                    <Form.Check
+                        type="checkbox"
+                        onChange={() => updateEvent(item.id, 'done', !item.done)}
+                        checked={item.done}
+                        label={item.title}
+                    />
+                    <Pencil className="right ml-auto" onClick={() => setEdit(!edit)}/>
+                    <Trash onClick={() => deleteEvent(item.id)} className={'ml-3'}/>
+                </>}
+        </ListGroup.Item>
+    )
 
+}
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,7 +1,8 @@
 import React, {useState} from "react";
-import {Alert, Button, Col, Container, Form, ListGroup, Row} from 'react-bootstrap';
+import {Alert, Col, Container, Form, ListGroup, Row} from 'react-bootstrap';
 import {eventsData} from "../utils/data";
 import {Event} from "../components/items/Event";
+import {EventForm} from "../components/blocks/EventForm";
 
 export const Main = () => {
     const [events, setEvents] = useState(eventsData)
@@ -26,44 +27,40 @@ export const Main = () => {
         setTitle('')
     }
 
-
     return (
-        <div className={'mt-5'}>
-            <Container>
-                <Row>
-                    <Col xs={12} md={7}>
+        <Container className={'mt-5'}>
+            <Row>
+                <Col xs={12} md={7}>
+                    <ListGroup as="ul">
+                        <Form.Control
+                            type="text"
+                            className={'mb-3'}
+                            placeholder="Search for events"
+                        />
                         <Alert variant={'secondary'}>
-                            You have {events.length} things to do
+                            {events.length === 0
+                                ? 'There are no items.'
+                                : `You have ${events.length} things to do`
+                            }
                         </Alert>
-                        <ListGroup as="ul">
-                            {events.map(event => (
-                                <Event
-                                    key={event.id}
-                                    item={event}
-                                    updateEvent={updateEvent}
-                                    deleteEvent={deleteEvent}
-                                />
-                            ))}
-                        </ListGroup>
-                    </Col>
-                    <Col xs={6} md={5}>
-                        <Form.Group>
-                            <Form.Label>Event Name</Form.Label>
-                            <Form.Control
-                                value={title}
-                                onChange={event => setTitle(event.target.value)}
-                                type="text"
-                                placeholder="I need to..."
+                        {events.map(event => (
+                            <Event
+                                key={event.id}
+                                item={event}
+                                updateEvent={updateEvent}
+                                deleteEvent={deleteEvent}
                             />
-                        </Form.Group>
-                        <Button variant="primary"
-                                disabled={title.trim().length === 0}
-                                onClick={createEvent}>
-                            Create Event
-                        </Button>
-                    </Col>
-                </Row>
-            </Container>
-        </div>
+                        ))}
+                    </ListGroup>
+                </Col>
+                <Col xs={6} md={5}>
+                    <EventForm
+                        title={title}
+                        setTitle={setTitle}
+                        createEvent={createEvent}
+                    />
+                </Col>
+            </Row>
+        </Container>
     )
 }
